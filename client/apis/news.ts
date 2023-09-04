47949c36e4cb544ebbc6ca4371eeeeabd3bd1af3
Tyrone Leef <tyroneleef23@gmail.com>
2023-09-04 21:59:58 +1200

    Moved API key to env file

--- client/apis/news.ts
+++ client/apis/news.ts
@@ -1,16 +1,13 @@
 import request from 'superagent'
-import * as dotenv from 'dotenv'
 import { News } from '../../models/news'
-
-// dotenv.config()
 
 const rootUrl = '/api/v1'
 
-// const apiKey = process.env.newsKey
-
 export async function getAllNews(): Promise<News> {
   const response = await request.get(
-    `https://newsapi.org/v2/everything?q=keyword&apiKey=15701971b3874cca84c1d8503edfc8a4`,
+    `https://newsapi.org/v2/everything?q=keyword&apiKey=${
+      import.meta.env.VITE_API_KEY
+    }`,
   )
   return response.body
 }
