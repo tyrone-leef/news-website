7d68f88819c321b142c85efe2a2eceb46633ecc1
Tyrone Leef <tyroneleef23@gmail.com>
2023-09-05 11:42:45 +1200

    Add sort

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from '@tanstack/react-query'
 import { getAllNews } from '../apis/news'
+import { useState } from 'react'
 
 function formatDate(dateString: string) {
   const date = new Date(dateString)
@@ -14,15 +15,44 @@ function formatDate(dateString: string) {
 }
 
 function App() {
+  const sortArr = ['relevancy', 'popularity', 'publishedAt']
+  const [sort, setSort] = useState(sortArr[0])
+
   const { data, isLoading, isError } = useQuery({
-    queryKey: ['news'],
-    queryFn: () => getAllNews(),
+    queryKey: ['news', sort],
+    queryFn: () => getAllNews(sort),
   })
+
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedSort = e.target.value
+    setSort(selectedSort)
+  }
 
   console.log(data)
   return (
     <>
       <h1 className="text-red-700 text-6xl font-bold text-center m-5">News</h1>
+      <div className="flex flex-col mx-20 w-40">
+        <label
+          htmlFor="sort"
+          className="block mb-2 text-lg text-gray-900 font-medium"
+        >
+          Sort
+        </label>
+        <select
+          id="sort"
+          value={sort}
+          onChange={handleChange}
+          className="border border-gray-300 text-gray-900 text-lg focus:ring-blue-500 focus:border-blue-500 p-2.5"
+        >
+          {sortArr &&
+            sortArr.map((s) => (
+              <option key={s} value={s}>
+                {s}
+              </option>
+            ))}
+        </select>
+      </div>
       <div className="flex justify-center">
         <p>{isLoading ? <span className="loader"></span> : ''}</p>
         <p>{isError ? 'something went wrong' : ''}</p>
