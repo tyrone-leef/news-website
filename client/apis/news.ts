5f344f383c3b6cc4bfe3fb28f9b82b85688e6b32
Tyrone Leef <tyroneleef23@gmail.com>
2023-09-04 20:54:44 +1200

    Data validation

--- client/apis/news.ts
+++ client/apis/news.ts
@@ -8,9 +8,9 @@ const rootUrl = '/api/v1'
 
 // const apiKey = process.env.newsKey
 
-export async function getAllNews(category: string): Promise<News> {
+export async function getAllNews(): Promise<News> {
   const response = await request.get(
-    `https://newsapi.org/v2/top-headlines/sources?category=${category}apiKey=15701971b3874cca84c1d8503edfc8a4`,
+    `https://newsapi.org/v2/everything?q=keyword&apiKey=15701971b3874cca84c1d8503edfc8a4`,
   )
   return response.body
 }
--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,11 +1,24 @@
 import { useQuery } from '@tanstack/react-query'
 import { getAllNews } from '../apis/news'
 
+function formatDate(dateString: string) {
+  const date = new Date(dateString)
+
+  const options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  } as const
+
+  return new Intl.DateTimeFormat('en-US', options).format(date)
+}
+
 function App() {
   const { data } = useQuery({
     queryKey: ['news'],
     queryFn: () => getAllNews(),
   })
+
   console.log(data)
   return (
     <>
@@ -14,31 +27,43 @@ function App() {
         {data &&
           data.articles.map((n, index) => (
             <li key={index}>
-              <div className="text-3xl mx-48 my-16 p-16  border-2 border-black flex flex-col justify-center items-center gap-10">
-                <h2 className="text-4xl font-bold hover:text-pink-700">
-                  <a href={n.url}>{n.title}</a>
-                </h2>
-                <p>
-                  <b>Author: </b>
-                  {n.author}
-                </p>
-                <p>
-                  <b>Description: </b>
-                  {n.description}
-                </p>
-                <p>
-                  <b>Publish Date: </b>
-                  {n.publishedAt}
-                </p>
-                <a className="hover:text-pink-700" href={n.url}>
-                  Read more
-                </a>
-                <img
-                  className="h-auto max-w-xl border-2 border-black"
-                  src={n.urlToImage}
-                  alt="news-related"
-                />
-              </div>
+              {n.title === '[Removed]' ? (
+                ''
+              ) : (
+                <div className="text-3xl mx-48 my-16 p-16  border-2 border-black flex flex-col justify-center items-center gap-10">
+                  <h2 className="text-4xl font-bold hover:text-pink-700">
+                    <a href={n.url}>{n.title}</a>
+                  </h2>
+                  {n.author === null ? (
+                    ''
+                  ) : (
+                    <p>
+                      <b>Author: </b>
+                      {n.author}
+                    </p>
+                  )}
+                  <p>
+                    <b>Description: </b>
+                    {n.description}
+                  </p>
+                  <p>
+                    <b>Publish Date: </b>
+                    {formatDate(n.publishedAt)}
+                  </p>
+                  <a className="hover:text-pink-700" href={n.url}>
+                    Read more
+                  </a>
+                  {n.urlToImage === null ? (
+                    ''
+                  ) : (
+                    <img
+                      className="h-auto max-w-xl border-2 border-black"
+                      src={n.urlToImage}
+                      alt="news article"
+                    />
+                  )}
+                </div>
+              )}
             </li>
           ))}
       </ul>
