ea0c76bb3023bbf642199a7cc984a179ae3fa9ff
Tyrone Leef <tyroneleef23@gmail.com>
2023-09-05 13:29:05 +1200

    Implement search function

--- client/apis/news.ts
+++ client/apis/news.ts
@@ -3,9 +3,9 @@ import { News } from '../../models/news'
 
 const rootUrl = '/api/v1'
 
-export async function getAllNews(sort: string): Promise<News> {
+export async function getAllNews(sort: string, search: string): Promise<News> {
   const response = await request.get(
-    `https://newsapi.org/v2/everything?q=keyword&sortBy=${sort}&apiKey=${
+    `https://newsapi.org/v2/everything?q=${search}&sortBy=${sort}&apiKey=${
       import.meta.env.VITE_API_KEY
     }`,
   )
